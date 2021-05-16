99ed784c2e3441ddcc0b9b06b20606e83702ded6
KarinaOknup <punko.karina@yandex.by>
2021-05-16 17:54:28 +0300

    add: start creating components

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header(props) {
+
+  return (
+    <header className = 'header'>
+      <img src="../../gitLogo.png" alt="git_logo" className="gitLogo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ my-app/src/components/Main/Start/Start.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Start.css';
+
+
+function Start(props) {
+
+  return (
+    <div className = 'start'>
+      ddddddd
+    </div>
+  );
+}
+
+export default Start;
