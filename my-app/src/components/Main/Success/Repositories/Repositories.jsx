a38ecb6c37dc7a677af7b48be938eb200ac83f8a
KarinaOknup <punko.karina@yandex.by>
2021-05-21 19:53:31 +0300

    add: pagination

--- my-app/src/components/Main/Success/Repositories/Repositories.jsx
+++ my-app/src/components/Main/Success/Repositories/Repositories.jsx
@@ -1,20 +1,47 @@
 import React from 'react';
+import ReactPaginate from "react-paginate";
+import { useState } from 'react';
 import './Repositories.css';
 import RepItem from './RepItem/RepItem';
 
+const PER_PAGE = 4;
 
 function Repositories(props) {
-  console.log(props.repos);
-    let repositories = props.repos.map(repItem =><RepItem description = {repItem.description}  url = {repItem.svn_url} name = {repItem.name} key = {repItem.id}/>)
+  const [currentPage, setCurrentPage] = useState(0);
+
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
+  const offset = currentPage * PER_PAGE;
+  const currentPageData = props.repos.slice(offset, offset + PER_PAGE)
+      .map(repItem => <RepItem
+                        description = {repItem.description}
+                        url = {repItem.svn_url}
+                        name = {repItem.name}
+                        key = {repItem.id}/>
+                        );
+
+  const pageCount = Math.ceil(props.repos.length / PER_PAGE);
+
   return (
       <div className = 'repositories'>
       <h2>Repositories ({props.repos.length})</h2>
       <div className="rep-box">
-      {repositories}
+         {currentPageData}
       </div>
-      <div className="pagination-box">
-        <span>кол-во показанных</span>
-        <div className="pagination">Здесь будет пагинация) react-paginate</div>
+      <div className='pag-box'>
+        <span>{ pageCount==currentPage?props.repos.length : (currentPage+1)*4}/{props.repos.length}</span>
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          previousLinkClassName={"pag-nav"}
+          nextLinkClassName={"pag-nav"}
+          disabledClassName={"pag-nav--disabled"}
+          activeClassName={"pag-nav--active"}
+        />
       </div>
     </div>
   );
